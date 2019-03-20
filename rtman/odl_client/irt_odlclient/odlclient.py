b4b32bec119d2acff4bae781d67a183b02caddfd
Alissa Gerhard <git@alissagerhard.de>
2019-03-20 11:02:30 +0100

    Can now disable drop-all-udp-traffic (default:disabled)

--- rtman/odl_client/irt_odlclient/odlclient.py
+++ rtman/odl_client/irt_odlclient/odlclient.py
@@ -17,17 +17,21 @@ class IRTOdlClient(ReservingODLClient):
 
         "_tas_handler",
 
-        "_scheduler"
+        "_scheduler",
+
+        "_drop_nonirt_udp_traffic"
     )
 
     _host_type = CapacityBasedHost
     _switch_type = CapacityBasedSwitch
 
-    def __init__(self, scheduler_cls, tas_handler=None, *args, **kwargs):
+    def __init__(self, scheduler_cls, tas_handler=None, drop_nonirt_udp_traffic=False, *args, **kwargs):
+        self._flows_dropnonregistered = set()
         super(IRTOdlClient, self).__init__(*args, **kwargs)
         self._scheduler = scheduler_cls(self, IRT_FLOW_PRIORITY)  # type: Scheduler
         self._tas_handler = tas_handler if tas_handler else TASHandler()
         self._tas_handler.start(self)
+        self._drop_nonirt_udp_traffic = drop_nonirt_udp_traffic
 
     def _generate_flowset(self):
         configuration = self._scheduler.calculate_new_configuration()
@@ -53,16 +57,17 @@ class IRTOdlClient(ReservingODLClient):
         topology_change_detected = super(IRTOdlClient, self)._build_nodes()
 
         # for each switch, add a flow that drops all IRT frames
-        self._flows_dropnonregistered = set(
-            FlowTableEntry(
-                switch=switch,
-                match=BaseMatch(ip_protocol=IPPROTOCOL_UDP),
-                instructions=(Actions((DropFrameAction(),)),),
-                priority=IRT_FLOW_PRIORITY-1,
-                entry_name="%s::drop_non_IRT" % self._flow_namespace
+        if self._drop_nonirt_udp_traffic:
+            self._flows_dropnonregistered = set(
+                FlowTableEntry(
+                    switch=switch,
+                    match=BaseMatch(ip_protocol=IPPROTOCOL_UDP),
+                    instructions=(Actions((DropFrameAction(),)),),
+                    priority=IRT_FLOW_PRIORITY-1,
+                    entry_name="%s::drop_non_IRT" % self._flow_namespace
+                )
+                for switch in self._switches.values()
             )
-            for switch in self._switches.values()
-        )
 
         if topology_change_detected:
             self._scheduler.init_nodestructure()
